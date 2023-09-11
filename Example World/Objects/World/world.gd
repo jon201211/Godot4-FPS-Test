aa85e17483e33c584bf2933242238862be4a9723
yanjun.gong <yanjun.gong@touchgfx.com.cn>
2023-09-11 18:55:04 +0800

    multiplayer & addons

--- Example World/Objects/World/world.gd
+++ Example World/Objects/World/world.gd
@@ -17,12 +17,30 @@ func _init():
 
 
 func _ready():
+	# We only need to spawn players on the server.
+	if not multiplayer.is_server():
+		return
+
 	multiplayer.peer_connected.connect(self.add_player)
 	multiplayer.peer_disconnected.connect(self.remove_player)
 	#multiplayer.server_disconnected.connect(self._close_network)
 	#multiplayer.connection_failed.connect(self._close_network)
 	#multiplayer.connected_to_server.connect(self._connected)
 
+	# Spawn already connected players.
+	for id in multiplayer.get_peers():
+		add_player(id)
+
+	# Spawn the local player unless this is a dedicated server export.
+	if not OS.has_feature("dedicated_server"):
+		add_player(1)
+
+
+func _exit_tree():
+	if not multiplayer.is_server():
+		return
+	multiplayer.peer_connected.disconnect(self.add_player)
+	multiplayer.peer_disconnected.disconnect(self.remove_player)
 
 
 #func _unhandled_input(event):
@@ -50,8 +68,13 @@ func _on_join_button_pressed():
 
 func add_player(peer_id):
 	var player = Player.instantiate()
+	# Set player id.
+	player.player_id = id
 	player.name = str(peer_id)
-	player.position.y = 3
+	#player.position.y = 3
+	# Randomize character position.
+	var pos := Vector2.from_angle(randf() * 2 * PI)
+	player.position = Vector3(pos.x * SPAWN_RANDOM * randf(), 0, pos.y * SPAWN_RANDOM * randf())
 	add_child(player)
 	if player.is_multiplayer_authority():
 		player.health_changed.connect(update_health_bar)
--- Player_Controller/scripts/Player_Character/player_character.gd
+++ Player_Controller/scripts/Player_Character/player_character.gd
@@ -4,6 +4,17 @@ signal health_changed(health_value)
 
 @onready var Camera = get_node("%Camera")
 
+
+# Set by the authority, synchronized on spawn.
+@export var player_id := 1 :
+	set(id):
+		player_id = id
+		# Give authority over the player input to the appropriate peer.
+		$PlayerInput.set_multiplayer_authority(id)
+
+# Player synchronized input.
+@onready var input = $PlayerInput
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -70,17 +81,35 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+
+	# Handle jump.
+	if input.jumping and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# Reset jump state.
+	input.jumping = false
+
+	# Handle movement.
+	var direction = (transform.basis * Vector3(input.direction.x, 0, input.direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+
+	# Handle Jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	velocity.x = move_toward(velocity.x, direction.x * SPEED, SPEED)
-	velocity.z = move_toward(velocity.z, direction.z * SPEED, SPEED)
+	# velocity.x = move_toward(velocity.x, direction.x * SPEED, SPEED)
+	# velocity.z = move_toward(velocity.z, direction.z * SPEED, SPEED)
 
 	move_and_slide()
 	
